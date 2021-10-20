53ccd8fdc0da38cfd28a858602d2c216162d1c5b
vitorqueirosz <vitor.queiroz16@hotmail.com>
2021-10-19 22:25:40 -0300

    chore: add QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from 'styles/global';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import theme from 'styles/theme';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 export default MyApp;
